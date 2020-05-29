868111b4c95d0e527e221693017a8b318c44a624
unknown <jeffynho32@gmail.com>
2020-05-29 13:08:52 -0300

    config: history

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 
 import GlobalStyle from './styles/global';
+import history from './services/history';
 
 import Routes from './routes';
 
 export default function App() {
   return (
-    <>
+    <Router history={history}>
       <Routes />
       <GlobalStyle />
-    </>
+    </Router>
   );
 }
