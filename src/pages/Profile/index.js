84426069724c64932bae74ac0aef1d65040d3506
unknown <jeffynho32@gmail.com>
2020-05-29 13:52:10 -0300

    update: pages/Profile - estilos de formulários/Feature handle-submit/signOut

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { signOut } from '../../store/module/auth/actions';
+import { updateProfileRequest } from '../../store/module/user/actions';
 
-function Profile() {
-  return <div />;
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
+
+export default function ProFile() {
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de email" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme sua senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
+    </Container>
+  );
 }
-
-export default Profile;
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,5 +1,67 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.div`
-  
+  max-width: 900px;
+  margin: 50px auto;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      padding: 0 15px;
+      height: 44px;
+      margin: 0 0 15px;
+      color: #fff;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    hr {
+      border: 0;
+      height: 1px;
+      background: rgba(0, 0, 0, 0.2);
+      margin: 10px 0 20px;
+    }
+
+    button {
+      background: #3b9eff;
+      border: 0;
+      border-radius: 4px;
+      margin: 5px 0 0;
+      height: 44px;
+      color: #fff;
+      font-weight: bold;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.05, '#3b9eff')};
+      }
+    }
+  }
+
+  > button {
+    width: 100%;
+    background: #f64c75;
+    border: 0;
+    border-radius: 4px;
+    margin: 10px 0 0;
+    height: 44px;
+    color: #fff;
+    font-weight: bold;
+    font-size: 16px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.08, '#f64c75')};
+    }
+  }
 `;
