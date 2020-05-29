8337fb83df14b380302fd6ba0cbed09e09988b48
unknown <jeffynho32@gmail.com>
2020-05-29 14:04:41 -0300

    uptade: pages/signUp - add Feature handleSubmit (registra um novo usuário)

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signUpRequest } from '../../store/module/auth/actions';
 
 import logo from '../../assets/logo.svg';
 
@@ -16,11 +19,17 @@ const schema = Yup.object().shape({
 });
 
 function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form schema={schema} onSubmit={() => {}}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" autoComplete="off" />
         <Input
           name="email"
