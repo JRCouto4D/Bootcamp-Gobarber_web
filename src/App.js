f9f0c455ebaf0ca7237cb8612403b61940d72ec1
unknown <jeffynho32@gmail.com>
2020-05-29 12:40:09 -0300

    config: estilos globais

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import './config/ReactotronConfig';
 
+import GlobalStyle from './styles/global';
+
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
+  );
 }
